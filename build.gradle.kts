bfb4b600b073b3298c5327f47cf24028bf0ea382
SebaSphere <SebaSphereEmails@gmail.com>
2024-07-26 21:41:52 -0400

    chore: another test commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -110,9 +110,9 @@ gradlePlugin {
 
 publishing {
     repositories {
-
-        if (System.getenv().containsKey("NEXUS_REPOSITORY_URL")) {
-            maven(System.getenv("NEXUS_REPOSITORY_URL")!!) {
+        val nexusUrl = System.getenv("NEXUS_REPOSITORY_URL")
+        if (nexusUrl != null && !nexusUrl.startsWith("file")) {
+            maven(nexusUrl) {
                 credentials {
                     username = System.getenv("NEXUS_USER")
                     password = System.getenv("NEXUS_PASSWORD")
